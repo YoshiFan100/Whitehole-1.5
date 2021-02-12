d4a200dbfa914bb2e12ca189705e7f6121c91445
brandon47167 <superyoshifan101@gmail.com>
2021-02-12 13:00:32 -0800

    Test

--- src/com/aurum/whitehole/Whitehole.java
+++ src/com/aurum/whitehole/Whitehole.java
@@ -11,6 +11,7 @@
 
     You should have received a copy of the GNU General Public License along 
     with Whitehole. If not, see http://www.gnu.org/licenses/.
+    Yoshifan101 made the edits after forking.
 */
 
 package com.aurum.whitehole;
